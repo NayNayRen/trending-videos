a61237276219e4358e91b4fa3abb271502307a80
Nate <jeditendencies35@gmail.com>
2024-08-13 15:06:22 -0400

    continuing the new project

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,7 @@
 // Slot refers to and renders the current child route(index.jsx)
 // Stack allows for the stacking of pages for routing purposes
 import { Text } from "react-native";
-import { Slot, Stack } from "expo-router";
+import { Stack } from "expo-router";
 
 const RootLayout = () => {
 	return (

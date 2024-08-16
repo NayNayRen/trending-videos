0e83a547b818a2d4674a949b5495c3791a450523
Nate <jeditendencies35@gmail.com>
2024-08-16 16:58:09 -0400

    starting to get video cards added

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,5 +1,6 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import { icons } from "../constants";
 
 const VideoCard = ({
 	video: {
@@ -11,7 +12,34 @@ const VideoCard = ({
 }) => {
 	return (
 		<View className="flex-col items-center px-4 mb-10">
-			<Text className="text-xl text-white">{title}</Text>
+			<View className="flex-row gap-3 items-start">
+				<View className="justify-center items-center flex-row flex-1">
+					<View className="w-[45px] h-[45px] rounded-lg border border-secondary justify-center items-center">
+						<Image
+							source={{ uri: avatar }}
+							className="w-full h-full rounded-lg"
+							resizeMode="cover"
+						/>
+					</View>
+					<View className="justify-center flex-1 ml-3 gap-y-1">
+						<Text
+							className="text-xl text-white font-psemibold"
+							numberOfLines={1}
+						>
+							{title}
+						</Text>
+						<Text
+							className="text-sm text-gray-100 font-pregular"
+							numberOfLines={1}
+						>
+							{username}
+						</Text>
+					</View>
+				</View>
+				<View className="pt-2">
+					<Image source={icons.menu} />
+				</View>
+			</View>
 		</View>
 	);
 };
